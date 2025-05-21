820e190a3fd3fd71254b864944754baaaa9a028c
A H Shobuj <ahshobuj09@gmail.com>
2025-05-21 16:25:30 +0600

    Controlled all uncaughtException, unhandledRejection

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
+import { Server } from 'http';
+
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
   } catch (error) {
@@ -15,3 +18,17 @@ async function main() {
 }
 
 main();
+
+process.on('unhandledRejection', (err) => {
+  console.log(`detected unhandledRejection, shuttting down...`, err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+});
+
+process.on('uncaughtException', (err) => {
+  console.log(`detected uncaughtException, shuttting down...`, err);
+  process.exit(1);
+});
