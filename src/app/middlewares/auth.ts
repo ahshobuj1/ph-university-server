4648069c6463c9cab1767e7643ff5d5519fbf55d
A H Shobuj <ahshobuj09@gmail.com>
2025-06-04 21:11:16 +0600

    created auth middlewere to verify jwt token, verify and set Authorization

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,44 @@
+import httpStatus from 'http-status';
+import { NextFunction, Request, Response } from 'express';
+import catchAsync from '../utils/catchAsync';
+import { AppError } from '../errors/AppError';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
+import { TUserRole } from '../modules/user/user.interface';
+
+// higher-order middleware func
+const auth = (...requiredRoles: TUserRole[]) => {
+  //middleware func
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    // check if token is undefined
+
+    const token = req.headers.authorization;
+    if (!token) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
+    }
+
+    // verify token
+    const decoded = jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+    ) as JwtPayload;
+
+    // decoded user role and id
+    const {
+      jwtPayload: { role },
+    } = decoded;
+
+    // check authorization access by role
+    if (requiredRoles && !requiredRoles.includes(role)) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+    }
+
+    // console.log(decoded, 'from auth -> ', req.headers.authorization);
+
+    // set user to express req
+    req.user = decoded;
+    next();
+  });
+};
+
+export default auth;
--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -4,23 +4,27 @@ import { Router } from 'express';
 import validationChecker from '../../middlewares/validationChecker';
 import { facultyValidations } from '../faculty/faculty.validation';
 import { adminValidations } from '../Admin/admin.validation';
+import auth from '../../middlewares/auth';
+import { UserRole } from './user.constant';
 
 const router = Router();
 
 router.post(
   '/create-student',
+  auth(UserRole.admin),
   validationChecker(studentValidations.createStudentValidationsSchema),
   userController.createStudent,
 );
 
 router.post(
   '/create-faculty',
+  auth(UserRole.admin),
   validationChecker(facultyValidations.createFacultyValidation),
   userController.createFaculty,
 );
 
 router.post(
-  '/create-admin',
+  '/create-admin', //super admin
   validationChecker(adminValidations.createAdminValidation),
   userController.createAdmin,
 );
